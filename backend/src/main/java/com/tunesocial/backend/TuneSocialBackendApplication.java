60501f5db2229f5e45931af5590bb7488ba8a3a1
Kacper Dęga <kacperdgf@o2.pl>
2025-12-05 14:32:46 +0100

    refactor(backend): Migrate backend from NestJS to Spring Boot

---
+++ backend/src/main/java/com/tunesocial/backend/TuneSocialBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tunesocial.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuneSocialBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuneSocialBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tunesocial/backend/TuneSocialBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tunesocial.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuneSocialBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
